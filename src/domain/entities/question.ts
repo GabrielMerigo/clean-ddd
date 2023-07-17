a5d3d405fe5bd80fa5ca4b3684e638f5b5d9a1bb
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-07-17 08:38:22 -0300

    adding props to answer and questions entities

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 import { Slug } from "./values-objects/slug";
 
 interface QuestionProps {
   title: string;
   content: string;
   slug: Slug;
-  authorId: string;
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
+  createdAt: Date;
+  updateAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {}
