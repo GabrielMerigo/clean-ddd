4e12ff848e9ec14fb1a3fecc5c307204c5fab2c8
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-07-17 08:55:18 -0300

    abstracting

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 import { Answer } from "../entities/answer";
 import { AnswersRepository } from "../repositories/answer-repository";
 
@@ -15,10 +16,10 @@ export class AnswerQuestionUseCase {
     questionId,
     content,
   }: IAnswerQuestionUseCaseProps) {
-    const answer = new Answer({
-      authorId: instructorId,
+    const answer = Answer.create({
       content,
-      questionId,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
     });
 
     await this.answersRepository.create(answer);
