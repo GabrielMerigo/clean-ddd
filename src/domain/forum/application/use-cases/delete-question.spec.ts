5afda63ad19ab76a633b5429d17cfeccba4b0a87
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-07-26 11:32:31 -0300

    delete-question use case done!

--- src/domain/forum/application/use-cases/delete-question.spec.ts
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -12,16 +12,39 @@ describe("Delete Question", () => {
     sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository); // system under test
   });
 
-  test("should be able to get a question by slug", async () => {
-    const newQuestion = makeQuestion({}, new UniqueEntityID("question-1"));
+  test("should be able to delete a question", async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID("author-1"),
+      },
+      new UniqueEntityID("question-1")
+    );
 
     inMemoryQuestionsRepository.create(newQuestion);
 
     await sut.execute({
       questionId: "question-1",
+      authorId: "author-1",
     });
 
-    expect(question.id).toBeTruthy();
-    expect(question.slug).toEqual(newQuestion.slug);
+    expect(inMemoryQuestionsRepository.items).toHaveLength(0);
+  });
+
+  test("should not be able to delete a question from another user", async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID("author-1"),
+      },
+      new UniqueEntityID("question-1")
+    );
+
+    inMemoryQuestionsRepository.create(newQuestion);
+
+    expect(() => {
+      return sut.execute({
+        questionId: "question-1",
+        authorId: "author-2",
+      });
+    }).rejects.toBeInstanceOf(Error);
   });
 });
--- src/domain/forum/application/use-cases/delete-question.ts
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -2,6 +2,7 @@ import { QuestionsRepository } from "../repositories/questions-repository";
 
 interface IDeleteQuestionUseCaseProps {
   questionId: string;
+  authorId: string;
 }
 
 interface IDeleteQuestionUseCaseResponse {}
@@ -11,11 +12,16 @@ export class DeleteQuestionUseCase {
 
   async execute({
     questionId,
+    authorId,
   }: IDeleteQuestionUseCaseProps): Promise<IDeleteQuestionUseCaseResponse> {
     const question = await this.questionsRepository.findById(questionId);
 
     if (!question) {
       throw new Error("Question not found.");
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error("Not Allowed.");
     }
 
     await this.questionsRepository.delete(question);
